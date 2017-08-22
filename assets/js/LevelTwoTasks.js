ad3939f14ffe7a9739d90aa41386dfdbd42faf01
Ashley Scott <admin@projectge.com>
2017-08-22 11:14:15 +0100

    Add next two JSON related tasks to LevelTwoTasks html document

--- assets/js/LevelTwoTasks.js
+++ assets/js/LevelTwoTasks.js
@@ -1,4 +1,8 @@
 var WorkBookL2 = {
+
+    jsonFeed: "",
+    jsonObject: {},
+
     iterationThree: function(maxNum, div3Text, div5Text) {
         for(var i = 1; i <= 100; i++) {
             if(i % 3 === 0) {
@@ -30,7 +34,7 @@ var WorkBookL2 = {
                 input += 1;
             }
 
-            if(input == 1) {
+            if(input === 1) {
                 console.log(input + " is now 1, ending task...");
                 $('#output').append("<p>" + input + " is now 1, ending task...</p>");
                 break;
@@ -44,7 +48,97 @@ var WorkBookL2 = {
 
     },
 
-    stringsFour: function() {
+    stringsFour: function(input) {
+        var text = input.split("");
+        $('#output').append("<p>" + text + " -> " + text.some(function(v,i,a) {
+            console.log(v + " " + i + " " + a);
+            $('#output').append("<p>" + v + " " + i + " " + a + "</p>");
+            return a.lastIndexOf(v) != i;
+        }) + "</p>");
+    },
 
+    createParagraph: function() {
+            $('#output').append("<p>Created new paragraph.</p>");
+        var block = document.getElementById("create-target");
+        block.innerHTML = "<p id=\"paragraph-target\">Test</p>";
+    },
+
+    saveParagraph: function(input) {
+        var block = document.getElementById("paragraph-target");
+
+        if(block) {
+            $('#output').append("<p>Saved text into paragraph.</p>");
+            block.innerHTML = input;
+        }
+            $('#output').append("<p>Paragraph does not exist, create it first.</p>");
+        }
+    },
+
+    deleteParagraph: function() {
+        $('#output').append("<p>Deleted paragraph.</p>");
+        var block = document.getElementById("paragraph-target");
+        block.remove();
+    },
+
+    jsonOne: function() {
+        var url = "https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json";
+
+        // Should use async
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, false);
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                WorkBookL2.jsonFeed = this.responseText;
+                WorkBookL2.jsonObject = JSON.parse(this.responseText);
+            }
+        };
+        xhr.send();
+
+        $('#output').append("<p>" + this.jsonFeed + "</p>");
+        console.log(this.jsonObject);
+
+        $('#render-target-heroes').append(
+            "<div><h4>" + this.jsonObject.squadName + "</h4>" + 
+            "<p><strong>Home Town</strong>:" + this.jsonObject.homeTown + "</p>" + 
+            "<p><strong>Secret Base</strong>:" + this.jsonObject.secretBase + "</p>" + 
+            "<p><strong>Formed</strong>:" + this.jsonObject.formed + "</p>" + 
+            "<p><strong>Active</strong>:" + (this.jsonObject.active ? "Yes" : "No") + "</p></div><br/><br/>"
+        );
+
+        $('#render-target-heroes').append("<h4>Team Members</h4>");
+        for(var player in this.jsonObject.members) {
+            $('#render-target-heroes').append(
+                "<div><p><strong>Player Name</strong>: " + this.jsonObject.members[player].name + "</p>" + 
+                "<p><strong>Player Age</strong>: " + this.jsonObject.members[player].age + "</p>" + 
+                "<p><strong>Player Secret Identity</strong>: " + this.jsonObject.members[player].secretIdentity + "</p>" + 
+                "<p><strong>Player Powers</strong>: " + this.jsonOneGetPowers(this.jsonObject.members[player].powers) + "</p></div><br/><br/>"
+            );
+        }
+    },
+
+    jsonOneGetPowers: function(powers) {
+        var final = "";
+        for(var i = 0; i < powers.length; i++) {
+            final += "<p>>> Power " + i + ": " + powers[i] + "</p>";
+        }
+        return final;
+    },
+
+    jsonTwo: function() {
+        var url = "https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/kings.json";
+
+        // Should use async
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, false);
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                WorkBookL2.jsonFeed = this.responseText;
+                WorkBookL2.jsonObject = JSON.parse(this.responseText);
+            }
+        };
+        xhr.send();
+
+        $('#output').append("<p>" + this.jsonFeed + "</p>");
+        console.log(this.jsonObject);
     }
 }

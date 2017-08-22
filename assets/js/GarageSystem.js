e72dcefd8b409b4fa60744774b4b5d65c3180259
Ashley Scott <admin@projectge.com>
2017-08-22 14:59:31 +0100

    Garage skeleton for adding vehicles to the list

---
+++ assets/js/GarageSystem.js
@@ -0,0 +1,52 @@
+var Vehicle = {
+    make: function(r, b, m, f) {
+        var v = {
+            reg: r,
+            brand: b,
+            model: m,
+            faults: f,
+
+            checkedIn: true,
+            checkIn: function() {
+                this.checkedIn = true;
+            },
+            checkOut: function() {
+                this.checkedIn = true;
+            },
+            serialise: function() { 
+                return "[" + this.reg + ", " + this.brand + ", " + this.model + ", " + this.faults + "]";
+            }
+        }
+
+        return v;
+    }
+}
+
+
+var GarageSystem = {
+    vehicleList: [],
+
+    addVehicle: function(reg, brand, model, faults) {
+        
+        var tempVehicle = Vehicle.make(reg, brand, model, faults);
+        $('#output').append("<p>Added vehicle: " + tempVehicle.serialise()  + "</p>");
+
+        vehicleList.push(tempVehicle);
+    },
+    
+    checkInVehicle: function(id) {
+        vehicleList[id].checkIn();
+    },
+
+    checkOutVehicle: function(id) {
+        vehicleList[id].checkOut();
+    },
+
+    printInventory: function() {
+
+    },
+
+    calculatePrice: function(id) {
+
+    }
+}

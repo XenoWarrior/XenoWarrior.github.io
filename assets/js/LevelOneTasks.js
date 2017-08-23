e479dc856b86e651cc802cccd6ee118442586295
Ashley Scott <admin@projectge.com>
2017-08-23 11:25:04 +0100

    Cleanup javascript and fix tabs to level one tasks

--- assets/js/LevelOneTasks.js
+++ assets/js/LevelOneTasks.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var WorkBookL1 = {
 
     personObject: { 
@@ -5,122 +7,113 @@ var WorkBookL1 = {
         age: 24,
         job: "Some Job"
     },
-
-    createScript: function() {
+    createScript: function () {
         alert("Hello World!");
     },
-
     variablesOutput: function () {
-        var someValue = "Level 1 - Basic - Variables";
+        let someValue = "Level 1 - Basic - Variables";
 
         window.alert(someValue);
         console.log(someValue);
 
-        $('#output').append("<p>" + someValue + "</p>");
+        $('#output').append(`<p>${someValue}</p>`);
 
         return someValue;
     },
-
     squareNumber: function(input) {
-        var res = input * input;
+        let res = input * input;
 
-        $('#output').append("<p>Input: " + input + ", Result: " + res + "</p>");
-
-        return res;
-    },
-
-    sumOfThree: function(a,b,c) {
-        var res = a + b + c;
-
-        $('#output').append("<p>Input: [" + a + ", " + b + ", " + c + "], Result: " + res + "</p>");
+        $('#output').append(`<p>Input: ${input}, Result: ${res}</p>`);
 
         return res;
     },
+    sumOfThree: function(a, b, c) {
+        let res = a + b + c;
 
-    createPerson: function() {
+        $('#output').append(`<p>Input: [${a}, ${b}, ${c}], Result: ${res}</p>`);
+
+        return res;
+    },
+    createPerson: function () {
+
         $('#output').append("<p>Created Person:</p>");
-        $('#output').append("<p>--- Name: " + this.personObject.name + "</p>");
-        $('#output').append("<p>--- Age: " + this.personObject.age + "</p>");
-        $('#output').append("<p>--- Job: " + this.personObject.job + "</p>");
+        $('#output').append(`<p>--- Name: ${this.personObject.name}</p>`);
+        $('#output').append(`<p>--- Age: ${this.personObject.age}</p>`);
+        $('#output').append(`<p>--- Job: ${this.personObject.job}</p>`);
 
         this.personObject.name = "New Name";
         this.personObject.age = 21;
         this.personObject.job = "New Job";
 
         $('#output').append("<br/><p>Edited Person:</p>");
-        $('#output').append("<p>--- Name: " + this.personObject.name + "</p>");
-        $('#output').append("<p>--- Age: " + this.personObject.age + "</p>");
-        $('#output').append("<p>--- Job: " + this.personObject.job + "</p>");
+        $('#output').append(`<p>--- Name: ${this.personObject.name}</p>`);
+        $('#output').append(`<p>--- Age: ${this.personObject.age}</p>`);
+        $('#output').append(`<p>--- Job: ${this.personObject.job}</p>`);
     },
-
-    incrementAge: function() {
-        var ageBefore = this.personObject.age;
+    incrementAge: function () {
+        let ageBefore = this.personObject.age;
         this.personObject.age += 1;
 
         $('#output').append("<br/><p>Increment Age:</p>");
-        $('#output').append("<p>--- Before: " + ageBefore + ", After: "+ this.personObject.age +"</p>");
+        $('#output').append(`<p>--- Before: ${ageBefore}, After: ${this.personObject.age}</p>`);
 
         return this.personObject.age;
     },
-
-    createPersonByForm: function() {
-        this.personObject.name = $('#l1_f3_e2_name').val();
-        this.personObject.age = parseInt($('#l1_f3_e2_age').val());
-        this.personObject.job = $('#l1_f3_e2_job').val();
+    createPersonByForm: function(n, a, j) {
+        this.personObject.name = n;
+        this.personObject.age = parseInt(a);
+        this.personObject.job = j;
 
         $('#output').append("<br/><p>Saving Person...</p>");
-        $('#output').append("<p>--- Name: " + this.personObject.name + "</p>");
-        $('#output').append("<p>--- Age: " + this.personObject.age + "</p>");
-        $('#output').append("<p>--- Job: " + this.personObject.job + "</p>");
+        $('#output').append(`<p>--- Name: ${this.personObject.name}</p>`);
+        $('#output').append(`<p>--- Age: ${this.personObject.age}</p>`);
+        $('#output').append(`<p>--- Job: ${this.personObject.job}</p>`);
     },
-    getPersonByForm: function() {
+    getPersonByForm: function () {
         $('#output').append("<br/><p>Current Person:</p>");
-        $('#output').append("<p>--- Name: " + this.personObject.name + "</p>");
-        $('#output').append("<p>--- Age: " + this.personObject.age + "</p>");
-        $('#output').append("<p>--- Job: " + this.personObject.job + "</p>");
+        $('#output').append(`<p>--- Name: ${this.personObject.name}</p>`);
+        $('#output').append(`<p>--- Age: ${this.personObject.age}</p>`);
+        $('#output').append(`<p>--- Job: ${this.personObject.job}</p>`);
     },
 
-    heSaidMyName: function() {
-        var val = "He said \"My name is Elliott\"";
+    heSaidMyName: function () {
+        let val = "He said \"My name is Elliott\"";
 
-        $('#output').append("<p>Current: " + val + "</p>");
-        $('#output').append("<p>New: " + val.toUpperCase() + "</p>");
+        $('#output').append(`<p>Current: ${val}</p>`);
+        $('#output').append(`<p>New: ${val.toUpperCase()}</p>`);
 
         return val;
     },
+    concatStringNum: function () {
+        let val = 'The String' + 123;
 
-    concatStringNum() {
-        var val = 'The String' + 123;
-
-        $('#output').append("<p>String + Number: " + val + "</p>");
+        $('#output').append(`<p>String + Number: ${val}</p>`);
 
         return val;
     },
+    arrayStringsTask: function () { 
+        let arr = ["Hello", "World", "!"];
 
-    arrayStringsTask: function() { 
-        var arr = ["Hello", "World", "!"];
-
-        $('#output').append("<p>Starting array: " + arr + "</p>");
-        for(var i = 0; i < arr.length; i++) {
-            $('#output').append("<p>--- String Array: " + arr[i] + "</p>");            
+        $('#output').append(`<p>Starting array: ${arr}</p>`);
+        for(let i = 0; i < arr.length; i++) {
+            $('#output').append(`<p>--- String Array: ${arr[i]}</p>`);
         }
 
         arr.push("Some new string added!");
 
-        $('#output').append("<p>Added new item: " + arr + "</p>");
-        for(var i = 0; i < arr.length; i++) {
-            $('#output').append("<p>--- String Array: " + arr[i] + "</p>");            
+        $('#output').append(`<p>Added new item to array: ${arr}</p>`);
+        for(let i = 0; i < arr.length; i++) {
+            $('#output').append(`<p>--- String Array: ${arr[i]}</p>`);          
         }
 
         delete arr.splice(arr.length-1,1);
 
-        $('#output').append("<p>Removing last item: " + arr + "</p>");
-        for(var i = 0; i < arr.length; i++) {
-            $('#output').append("<p>--- String Array: " + arr[i] + "</p>");            
+        $('#output').append(`<p>Removing last item from array: ${arr}</p>`);
+        for(let i = 0; i < arr.length; i++) {
+            $('#output').append(`<p>--- String Array: ${arr[i]}</p>`);          
         }
     },
-
-    checkAge: function() {
+    checkAge: function () {
         if(this.personObject.age > 20 && this.personObject.age < 40) {
             $('#output').append("<p>Person's age is between 20 and 40!</p>");
             return true;
@@ -128,19 +121,17 @@ var WorkBookL1 = {
         $('#output').append("<p>Person's age is not between 20 and 40!</p>");
         return false;
     },
-
-    iterationOne: function() {
+    iterationOne: function () {
         $('#output').append("<p>Incrementing</p>");
-        for(var i = 1; i <= 10; i++) {
-            $('#output').append("<p>--- " + i + "</p>");
+        for(let i = 1; i <= 10; i++) {
+            $('#output').append(`<p>--- ${i}</p>`);
         }
     },
-
-    iterationTwo: function() {
+    iterationTwo: function () {
         $('#output').append("<p>Checking if is divisible</p>");
-        for(var i = 1; i <= 10; i++) {
+        for(let i = 1; i <= 10; i++) {
             if(i % 2 === 0) {
-                $('#output').append("<p>--- " + i + "</p>");
+                $('#output').append(`<p>--- ${i}</p>`);
             }
         }
     }

b01ef7bfc5c4b69d90378c22e2bd4bd66b704361
Ashley Scott <admin@projectge.com>
2017-08-24 11:35:00 +0100

    Updates vehicle to follow a factory method

--- assets/js/data/Vehicle.js
+++ assets/js/data/Vehicle.js
@@ -1,29 +1,32 @@
 'use strict';
 
 var Vehicle = {
-    make: function (i, r, b, m, f, t) {
-        var v = {
-            id: i,
-            reg: r,
-            make: b,
-            model: m,
-            faults: f,
-            type: t ? t : "Unknown Type",
-
-            checkedIn: true,
-            checkIn: function () {
-                this.checkedIn = true;
-            },
-            checkOut: function () {
-                this.checkedIn = true;
-            },
-            setType: function(val) {
-                this.type = val;
-            },
-            serialiseVehicle: function () { 
-                return `[${this.id}, ${this.reg}, ${this.make}, ${this.model}, ${this.faults}, ${this.type}]`;
-            }
+    factory: function(type) { 
+        var make = function(i, r, b, m, f) {
+            return {
+                id: i,
+                reg: r,
+                make: b,
+                model: m,
+                faults: f,
+                type: type,
+    
+                checkedIn: true,
+                checkIn: function () {
+                    this.checkedIn = true;
+                },
+                checkOut: function () {
+                    this.checkedIn = true;
+                },
+                setType: function(val) {
+                    this.type = val;
+                },
+                serialiseVehicle: function () { 
+                    return `[${this.id}, ${this.reg}, ${this.make}, ${this.model}, [${this.faults}], ${this.type}]`;
+                }
+            };
         }
-        return v;
+        
+        return make;
     }
 };
